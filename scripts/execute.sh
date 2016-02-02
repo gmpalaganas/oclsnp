f0e37202fce1bec863e8dbdb467831d35192117f
Genesis Ian M.Palaganas <palaganas.genesis@gmail.com>
2016-02-02 12:20:43 +0800

    Add script for execution

---
+++ scripts/execute.sh
@@ -0,0 +1,10 @@
+cd ../bin
+./oclsnp ../inputs/2input_sort.bin ../outputs/2input_sort_ocl_out.txt
+./oclsnp ../inputs/4input_sort.bin ../outputs/4input_sort_ocl_out.txt
+./oclsnp ../inputs/8input_sort.bin ../outputs/8input_sort_ocl_out.txt
+./oclsnp ../inputs/16input_sort.bin ../outputs/16input_sort_ocl_out.txt
+./linsnp ../inputs/2input_sort.bin ../outputs/2input_sort_lin_out.txt
+./linsnp ../inputs/4input_sort.bin ../outputs/4input_sort_lin_out.txt
+./linsnp ../inputs/8input_sort.bin ../outputs/8input_sort_lin_out.txt
+./linsnp ../inputs/16input_sort.bin ../outputs/16input_sort_lin_out.txt
+cd ../scripts
