72852d7d1059aa83c009bd593b2d4bd7ecf23e52
Genesis Ian Palaganas <palaganas.genesis@gmail.com>
2017-03-02 20:24:27 +0800

    Remove reduduntant defines

--- src/main.hpp
+++ src/main.hpp
@@ -15,16 +15,6 @@
 
 #include <emulator.hpp>
 
-#define VECTOR_ADD_SRC             "kernels/vector_add.cl"
-#define VECTOR_ELEM_MULT_SRC       "kernels/vector_elem_mult.cl"
-#define VECTOR_SELECTIVE_ADD_SRC   "kernels/vector_selective_add.cl"
-
-#define SNP_COMPUTE_NET_GAIN_SRC "kernels/snp_compute_net_gain.cl"
-#define SNP_DETERMINE_RULES_SRC  "kernels/snp_determine_rules.cl"
-#define SNP_POST_COMPUTE_SRC     "kernels/snp_post_compute.cl"
-#define SNP_RESET_SRC            "kernels/snp_reset.cl"
-#define SNP_SET_STATES_SRC       "kernels/snp_set_states.cl"
-
 namespace programFlags{
     enum ProgramFlags { SILENT };
 }
